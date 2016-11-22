38d2352bf8f3c9b724407fe788370796e276b588
van <fanyu2013@foxmail.com>
2016-11-22 11:31:27 +0800

    create liteLibrary module

--- example/src/main/java/com/fanyu/litelib/MainActivity.java
+++ example/src/main/java/com/fanyu/litelib/MainActivity.java
@@ -2,12 +2,17 @@ package com.fanyu.litelib;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.fanyu.litelibrary.util.TestUtil;
 
 public class MainActivity extends AppCompatActivity {
+    public final String TAG = this.getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, TestUtil.getHello());
     }
 }
---
+++ liteLibrary/src/main/java/com/fanyu/litelibrary/util/TestUtil.java
@@ -0,0 +1,12 @@
+package com.fanyu.litelibrary.util;
+
+/**
+ *
+ * Created by fanyu on 16/11/22.
+ */
+
+public class TestUtil {
+    public static String getHello(){
+        return "fanyu.net.cn";
+    }
+}

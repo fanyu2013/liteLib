d358adbe74d41f8ec872825c7c81fd93df39191c
van <fanyu2013@foxmail.com>
2016-12-08 23:49:35 +0800

    fix

---
+++ liteLibrary/src/main/java/com/fanyu/litelibrary/LiteObj.java
@@ -0,0 +1,12 @@
+package com.fanyu.litelibrary;
+
+import com.fanyu.litelibrary.net.Http;
+
+/**
+ * 项目的控制中心
+ * Created by fanyu on 16/12/8.
+ */
+
+public class LiteObj {
+    public static Http http;
+}
